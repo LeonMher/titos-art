40d2431dca1c28a6d3ad32c0e8ab6f0b1ee1ef10
mtsatinyan <m.tsatinyan@digitai.group>
2024-05-28 19:16:52 +0400

    implement profile picture fetching mechanism

--- src/components/ProfilePicture/ProfilePicture.tsx
+++ src/components/ProfilePicture/ProfilePicture.tsx
@@ -1,12 +1,12 @@
-import ProfilePic from "../../../public/profilePicture.jpg";
 import Image from "next/image";
 
 export const ProfilePicture = () => {
+  const img = "http://localhost:3001/profilepic.jpg";
   return (
     <div className="flex justify-end p-[40px]">
       <Image
         className="borderOval"
-        src={ProfilePic}
+        src={img}
         width={300}
         height={300}
         alt="asd"
