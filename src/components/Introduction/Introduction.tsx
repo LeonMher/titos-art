6b9c8b07988150e719ac0982fbf83fc5de17c1d5
mtsatinyan <m.tsatinyan@digitai.group>
2024-05-29 22:34:46 +0400

    add AOS animation library

--- src/components/Introduction/Introduction.tsx
+++ src/components/Introduction/Introduction.tsx
@@ -1,9 +1,17 @@
 "use client";
 
 import useAboutMe from "../../api/useAboutMe";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
 
 export function Introduction() {
   const { data, isLoading, isError, error } = useAboutMe();
+
+  useEffect(() => {
+    AOS.init();
+    AOS.refresh();
+  }, []);
 
   console.log(data, " data");
 
@@ -13,7 +21,7 @@ export function Introduction() {
     <div className="flex items-start justify-center flex-col w-[300px] gap-large">
       <p className="text-3xl text-black-100">{intro}</p>
       <p className="text-gray-600">{experience}</p>
-      <div className="flex gap-midium">
+      <div className="flex gap-midium" data-aos="fade-right">
         <button className="bg-yellow-500 hover:bg-yellow-300 text-white font-bold py-2 px-4 rounded p-[10px]">
           Projects
         </button>
